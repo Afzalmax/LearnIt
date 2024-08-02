c0ac1b8ee51c8e88b96059d4a6cb2a3f448f44d7
Afzalmax <rafzal0606@gmail.com>
2024-08-02 14:49:12 +0530

    fix

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -10,8 +10,8 @@ const Footer = () => {
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
               <a href="https://flowbite.com/" className="flex items-center">
-                  <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 me-3" alt="FlowBite Logo" />
-                  <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                  {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 me-3" alt="FlowBite Logo" /> */}
+                  <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">LearnIT</span>
               </a>
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
@@ -19,10 +19,10 @@ const Footer = () => {
                   <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
                   <ul className="text-gray-500 dark:text-gray-400 font-medium">
                       <li className="mb-4">
-                          <a href="https://flowbite.com/" className="hover:underline">GeeksforGeeks</a>
+                          <p className="hover:underline">GeeksforGeeks</p>
                       </li>
                       <li>
-                          <a href="https://tailwindcss.com/" className="hover:underline">Other users</a>
+                          <p className="hover:underline">Other users</p>
                       </li>
                   </ul>
               </div>
@@ -30,10 +30,10 @@ const Footer = () => {
                   <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Contents</h2>
                   <ul className="text-gray-500 dark:text-gray-400 font-medium">
                       <li className="mb-4">
-                          <a href="https://github.com/themesberg/flowbite" className="hover:underline ">Core</a>
+                          <p className="hover:underline ">Core</p>
                       </li>
                       <li>
-                          <a href="https://discord.gg/4eeurUVvTy" className="hover:underline">General</a>
+                          <p className="hover:underline">General</p>
                       </li>
                   </ul>
               </div>
